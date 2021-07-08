26d31fda201e2b5d9978e7692a9c08aa2520f83d
Soukaina ABU SHMAIS <sabushmais@norsys.fr>
2021-07-08 09:56:45 +0100

    updating init.sh

--- init.sh
+++ init.sh
@@ -2,15 +2,19 @@ echo ">> composer install"
 docker-compose run --rm php-ssl php -d memory_limit=-1 /usr/local/bin/composer install
 
 echo ">> Working on missing files and file permissions ..."
-mkdir /web/sites/default/files
-mkdir /web/sites/default/translations
+echo ">> chown -R $USER:www-data ."
+sudo chown -R $USER:www-data .
+sudo mkdir -p web/sites/default/files
+sudo mkdir -p web/sites/default/translations
 
-chmod a+w sites/default/files
-chmod -R 755 .
-chmod 444 web/sites/default/settings.php
+sudo chmod a+w web/sites/default/files
+sudo chmod -R 755 .
+sudo chmod -R 775 web/sites/default
+cp web/sites/default/default.settings.php web/sites/default/settings.php
+sudo chmod 444 web/sites/default/settings.php
 
 echo ">> Importing the initial database..."
-docker exec -i drupal-8-socle_db_1 mysql -unorsys2021 -p1 agrisudweb < database/20210707.sql
+#docker exec -i drupal-8-socle_db_1 mysql -unorsys2021 -p1 agrisudweb < db/bkup-20210707.sql
 
 echo ">> fin"
 
