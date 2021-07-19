d3aad8851f8303966431f24816454bdde56142b8
mehdi.rehoumi <emrehoumi@norsys.fr>
2021-07-19 20:08:37 +0100

    [feature-3vey53] l'ajout du gulp

---
+++ web/themes/custom/norsys_base/gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp'),
+  livereload = require('gulp-livereload'),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+  gulp.src('./src/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('./src/sass/**/*.scss', gulp.series('sass'));
+  gulp.watch(['./css/style.css', './**/*.html.twig', './js/*.js'], function (files) {
+    livereload.changed(files);
+  });
+});
