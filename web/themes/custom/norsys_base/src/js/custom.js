4dde75d645276a0562915135d2c8dea3bd08def8
Soukaina ABU SHMAIS <sabushmais@norsys.fr>
2021-12-23 16:19:22 +0000

    Feat responsive ipad

--- web/themes/custom/norsys_base/src/js/custom.js
+++ web/themes/custom/norsys_base/src/js/custom.js
@@ -17,41 +17,43 @@ jQuery(document).ready(function ($) {
   } else {
     $('#language-select').val("fr");
   }
+  if ($(window).width() > 600) {
     $(".slider-caption a").text("Découvrir");
     $(".slider-caption a").addClass("more-link");
-    $("#donate-link").animate({width:'toggle'},350);
-    $("#presse-link").animate({width:'toggle'},350);
+  }
+  $("#donate-link").animate({width: 'toggle'}, 350);
+  $("#presse-link").animate({width: 'toggle'}, 350);
 
-    // Managing redirection onclick on taxonomy terms in page 'nos réseaux et partenaires'
+  // Managing redirection onclick on taxonomy terms in page 'nos réseaux et partenaires'
   $("#views-bootstrap-liste-type-partenaires-block-1 .views-field a").on("click", function (e) {
     e.preventDefault();
-    window.location.href = baseUrl + "/liste-partenaires?f%5B0%5D=type_de_partenaire%3A"+this.href.substring(this.href.lastIndexOf('/') + 1);
+    window.location.href = baseUrl + "/liste-partenaires?f%5B0%5D=type_de_partenaire%3A" + this.href.substring(this.href.lastIndexOf('/') + 1);
   });
 
   $("#donate-key").on("click", function (e) {
-      $("#donate-link").animate({width:'toggle'},350);
-    });
-  $("#presse-key").on("click", function (e) {
-    $("#presse-link").animate({width:'toggle'},350);
+    $("#donate-link").animate({width: 'toggle'}, 350);
   });
-    $("#block-views-block-block-nos-partenaires-block-1 .content div").eq(1).addClass("row");
-    var pays = ["maroc", "bresil", "france", "senegal", "guinee-bissau", "malaisie", "cambodge", "laos", "vietnam", "chine",
-        "mayotte", "maurice", "madagascar", "rd-congo", "seychelles", "Indonesie", "mozambique", "gabon", "sao-tome",
-        "niger-2", "benin", "cote-ivoire", "haiti", "inde"];
-    var getUrl = window.location;
-    var baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
-    $.each(pays, function (index, value) {
-        $("#" + value).attr('title', $("#" + value).attr('inkscape:label'));
-        $("#" + value).tooltip();
+  $("#presse-key").on("click", function (e) {
+    $("#presse-link").animate({width: 'toggle'}, 350);
+  });
+  $("#block-views-block-block-nos-partenaires-block-1 .content div").eq(1).addClass("row");
+  var pays = ["maroc", "bresil", "france", "senegal", "guinee-bissau", "malaisie", "cambodge", "laos", "vietnam", "chine",
+    "mayotte", "maurice", "madagascar", "rd-congo", "seychelles", "Indonesie", "mozambique", "gabon", "sao-tome",
+    "niger-2", "benin", "cote-ivoire", "haiti", "inde"];
+  var getUrl = window.location;
+  var baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+  $.each(pays, function (index, value) {
+    $("#" + value).attr('title', $("#" + value).attr('inkscape:label'));
+    $("#" + value).tooltip();
 
-        $("#" + value).on("click", function (e) {
-            index = index + 1;
-            window.location.href = baseUrl + "/liste-actions?field_pays="+index+"#block-actions";
-        });
+    $("#" + value).on("click", function (e) {
+      index = index + 1;
+      window.location.href = baseUrl + "/liste-actions?field_pays=" + index + "#block-actions";
     });
+  });
 
-    //manage language change
-  $('#language-select').on('change', function() {
+  //manage language change
+  $('#language-select').on('change', function () {
     var getUrl = window.location;
     window.location.href = baseUrl + "/" + this.value;
   });
@@ -64,4 +66,22 @@ jQuery(document).ready(function ($) {
       $("#search-block-form .form-item").show();
     }
   });
+  var listPages = ["#views-bootstrap-nos-actualites-view-page-1 .field--name-body", "#views-bootstrap-liste-actions-page-1 .field--name-body"];
+  var caractersCount = 175;
+  if ($(window).width() < 992 || $(window).width() < 600) {
+    $.each(listPages, function (index, value) {
+      if (index == 1 || $(window).width() < 600) {
+        caractersCount = 82;
+      }
+      $(value).each(function () {
+        console.log($(this).text().substring(0, 100));
+        var lastElements = $(this).html().slice(-125);
+        console.log(lastElements);
+        $(this).text($(this).text().substring(0, caractersCount));
+        $(this).html($(this).html() + lastElements);
+      });
+    });
+  }
+
+
 });
