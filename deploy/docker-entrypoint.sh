7ec736c65b89701ec57feb905f7a398a906e409d
Mohamed Akouz <makouz@norsys.fr>
2022-01-17 16:02:50 +0000

    Update docker-entrypoint.sh

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -5,6 +5,5 @@ sed -i 's@\[DATABASE_HOST\]@'"$DATABASE_HOST"'@' .env.prod
 sed -i 's@\[MYSQL_USER\]@'"$MYSQL_USER"'@' .env.prod
 sed -i 's@\[MYSQL_PASSWORD\]@'"$MYSQL_PASSWORD"'@' .env.prod
 cat .env.prod > .env
-cp -R config/dev/files-backup/* web/site/default/files
+cp -R config/dev/files-bkup/* web/sites/default/files
 service apache2 restart && tail -F /var/log/apache2/access.log && bash
-#exec /upstream-entrypoint.sh "$@"
